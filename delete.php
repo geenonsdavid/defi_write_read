91f411b2127fe046fae1f622cfdd507e7008a0d5
Geenons david <geenonsdavid@gmail.com>
2023-05-04 21:29:07 +0200

    first commit

--- delete.php
+++ delete.php
@@ -1,10 +1,19 @@
 <?php
+
+// récupère le nom de la table à effacer
+$delete = $_GET['delete'];
+
 // récupère l'id du message à effacer
 $id = $_GET['id'];
+
 // connexion à la base de données
 include_once 'connexion.php';
+// selectionner la table games
+$pdo->exec('USE defi');
+
 // efface le message
-$pdo->exec("DELETE FROM messages WHERE id = $id");
+$pdo->exec("DELETE FROM $delete WHERE id = $id");
+
 // retourne à la page index.php
 header('Location: index.php');
 ?>
