6730a8348371f93e21dd3acb9ca49e828a12e759
Geenons david <geenonsdavid@gmail.com>
2023-05-04 21:32:33 +0200

    factorize file

--- index.php
+++ index.php
@@ -10,51 +10,59 @@
 </head>
 <body>
 <?php
+try {
+// connexion à la base de données
 include_once 'connexion.php';
-include_once 'Message.php';
+// selectionner la table games
+$pdo->exec('USE defi');
+// si l'id est défini
 
-?>
-<form class="w-25" method="POST" action="">
-<label for="message" class="form-label form-control-lg">Message :</label>
-<textarea name="message" id="message" cols="30" rows="1" class="form-control"></textarea>
-<button type="submit" class="btn btn-primary m-2">Envoyer</button>
-</form>
-<?php
+if (!empty($_GET['id'])){
+// récupérer l'id games
+    $sql = 'SELECT * FROM games WHERE id = :id';
+    $pdoStatement = $pdo->prepare($sql);
+    $pdoStatement->execute([
+        'id' => $_GET['id']
+    ]);
+    // si aucun jeu n'est trouvé
+     if ($pdoStatement->rowCount() === 0) {
+        echo 'Aucun jeu trouvé';
+        // get games
+        $pdoStatement = $pdo->query('SELECT * FROM games');
+        $games = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
+        include_once 'affichage.php';
+        }else{
+            // get games
+            $games = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
+            // show games in a card where id = $_GET['id']
+            echo '<div class="d-flex bg-dark w-25 flex-column justify-content-center">';
+            include_once 'affichage.php';
+            echo '<div class="d-flex align-self-center">';
+            echo '<a href=delete.php?delete=games&id='.$_GET['id'].' class="btn btn-danger">Supprimer</a>';
+            echo '</div>';
+            echo '</div>';
+            }  
+}else{
+    // récupérer les id des games
+    $pdoStatement = $pdo->query('SELECT * FROM games');
+    $games = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
 
-
-    if(empty($_POST["message"])){
-       // affiche alerte
-         echo "<div class='alert alert-danger w-25 m-2' role='alert'>Veuillez saisir un message</div>";
-    }
-    else {
-    // affiche alerte
-    echo "<div class='alert alert-success w-25 m-2' role='alert'>Message envoyé</div>";
-    // Ajout d'un message     
-    $pdo->exec("INSERT INTO messages (message) VALUES ('$_POST[message]')");
-    }
-
-// connaitre nombre de messages
-$request ='SELECT COUNT(*) AS nb_messages FROM messages';
-$pdoStatement = $pdo->prepare($request);
-$pdoStatement->execute();
-$nb_messages = $pdoStatement->fetch(PDO::FETCH_ASSOC);
-
-// Récupération des messages
-
-$request = $pdo->prepare('SELECT message,date,id FROM messages');
-$request->execute();
-
-// affichage des messages
-
-while ($message = $request->fetch(PDO::FETCH_ASSOC)) {
-    echo "<div class='card w-25 m-2'>";
-    echo "<div class='card-body'>";
-    echo "<p class='card-text'>" . $message['message'] . "</p>";
-    echo "<p class='card-text'>" . $message['date'] . "</p>";
-    echo "<a href='delete.php?id=" . $message['id'] . "' class='btn btn-danger'>Supprimer</a>";
-    echo "</div>";
-    echo "</div>";
+    // afficher les games
+    include_once 'affichage.php';
 }
+
+ 
+   
+} catch (PDOException $e) {
+
+    // envoyer l'erreur dans un fichier error.txt
+    file_put_contents('error.txt', $e->getMessage());
+    // afficher un message d'erreur
+    echo 'erreur de connexion à la base de données';
+
+
+}     
+
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
