d03e9b52adfe77e381feb79d1d04c74a02474d49
geenonsdavid <44168437+geenonsdavid@users.noreply.github.com>
2023-05-12 16:57:16 +0200

    sépare en MVC

--- index.php
+++ index.php
@@ -1,36 +1,7 @@
 <?php
-try{
-//connexion à la base de données
-require 'connexion.php';
-// chargement des classes
-include_once 'Message.php';
-require 'MessageManager.php';
+require 'src/model.php';
 
-/// créer class gestionnaire de message
-$messageManager = new MessageManager($pdo);
-
-// create database defi
-$pdo->exec("CREATE DATABASE IF NOT EXISTS defi");
-
-// use database defi
-$pdo->exec("USE defi");
-
-// create table messages
-include_once 'create_table_messages.php';
-
-
-}
-catch(PDOException $e){
-    
-    if ($e->getCode() == 1045){
-        echo "<div class='alert alert-danger w-25 m-2' role='alert'>Erreur de connexion à la base de données</div>";
-    }
-    else {
-        echo "<div class='alert alert-danger w-25 m-2' role='alert'>Erreur : " . $e->getMessage() . "</div>";
-    }
-}
-
-include_once 'homepage.php';
+include_once 'templates/homepage.php';
 ?>
 
 
