bfb1c7a0bab9ab9bb27b0a07e9472c05cc032e77
Geenons david <geenonsdavid@gmail.com>
2023-05-16 19:47:19 +0200

    connexion

--- connexion.php
+++ connexion.php
@@ -1,7 +1,7 @@
 <?PHP
-$host = "localhost";
+$host = "https://www.cleardb.com/service/1.0/api";
 $dbName = "defi";
 
 // Connexion à la base de données
 
-    $pdo = new PDO("mysql:host=$host;charset=utf8", "root", "1234");
+    $pdo = new PDO("mysql:host=$host;charset=utf8", "bf2d72951131cb", "Magicodu62&");
