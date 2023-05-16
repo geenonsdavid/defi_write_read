7d70265c73daff91a3dd1300be15ea1db4c2858d
Geenons david <geenonsdavid@gmail.com>
2023-05-16 20:01:18 +0200

    update

--- connexion.php
+++ connexion.php
@@ -4,4 +4,4 @@ $dbName = "defi";
 
 // Connexion à la base de données
 
-    $pdo = new PDO("mysql:host=$host;charset=utf8", "bf2d72951131cb", "Magicodu62&");
+    $pdo = new PDO("mysql:host=$host;charset=utf8", "bf2d72951131cb", "Oeae777d");
