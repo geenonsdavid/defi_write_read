0676614adf7cf3579df880fc93c42a5ac0db6f1e
Geenons david <geenonsdavid@gmail.com>
2023-05-17 08:45:19 +0200

    modify parameter connexion

--- connexion.php
+++ connexion.php
@@ -1,8 +1,8 @@
 <?PHP
 
-$host = "www.cleardb.com";
-$dbName = "defi";
+$host = "w3epjhex7h2ccjxx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbName = "wnqsbktgjcpzjw7c";
 
 // Connexion à la base de données
 
-    $pdo = new PDO("mysql:host=$host;charset=utf8", "bf2d72951131cb", "Oeae777d");
+    $pdo = new PDO("mysql:host=$host:3306;charset=utf8", "uqms2rkyh3a46suw", "t1nompiclwc62zvn");
